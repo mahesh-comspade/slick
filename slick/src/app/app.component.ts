fccf0222ba0763701b405d811c3e7b198acfaac9
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2018-05-22 17:07:40 +0530

    Slick worked

--- slick/src/app/app.component.ts
+++ slick/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+   slides = [
+    {img: "http://placehold.it/350x150/"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://localhost/1.png"}
+  ];
+  slideConfig = {"slidesToShow": 2, "slidesToScroll": 2, "dots": true, "infinite": true, "autoplay": true, "autoplaySpeed": 2000};
+
+  addSlide() {
+    this.slides.push({img: "https://avatars3.githubusercontent.com/u/1925733?s=88&v=4"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  afterChange(e) {
+    console.log('afterChange');
+  }
 }
--- slick/src/app/app.module.ts
+++ slick/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SlickModule } from 'ngx-slick';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SlickModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
